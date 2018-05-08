7a12fa731d84623f086ba11a4858870d76bfd97b
GermanAngarita <germanadolfoangarita@gmail.com>
2018-05-08 09:09:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     db: process.env.MONGODB || 'mongodb://heroku_kbw1v3zm:9jb9gq505ao51fmuevoh70j7j9@ds139964.mlab.com:39964/heroku_kbw1v3zm', 
     SECRET_TOKEN: 'mysecret'
 }
