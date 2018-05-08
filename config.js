343fb835f53a8c51c08f537e8568239de1460854
GermanAngarita <germanadolfoangarita@gmail.com>
2018-05-08 08:36:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost/27017/techrepair', 
+    db: process.env.MONGODB || 'mongodb://heroku_kbw1v3zm:9jb9gq505ao51fmuevoh70j7j9@ds139964.mlab.com:39964/heroku_kbw1v3zm', 
     SECRET_TOKEN: 'mysecret'
 }
+
+
