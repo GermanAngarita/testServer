30e5841929ef67ff698f7b538ce05957ed11d850
German Angarita <germanadolfoangarita@gmail.com>
2018-05-22 09:08:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://test.mibbu.com/");
+    res.header("Access-Control-Allow-Origin", "http://test.mibbu.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
