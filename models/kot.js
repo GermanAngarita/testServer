631a95b0d434f13f708611db5b1a35c7553d1e9b
German Angarita <germanadolfoangarita@gmail.com>
2018-06-06 09:22:42 -0500

    V 0.012

--- models/kot.js
+++ models/kot.js
@@ -6,7 +6,14 @@ const Schema = mongoose.Schema
 const kotSchema = new Schema({
     dealer:{ type:String },
     periodo:{ type:Number },
-    kot:{ type:Number }
+    ingresos:{type:Number},
+    kot:{ type:Number },
+    envios:{type:Number},
+    puntos:{type:Number},
+    fotos:{type:Number},
+    videos:{ type:Number},
+    tiempo_envio:{type:Number}
+
 })
 
 module.exports = mongoose.model( 'Kot', kotSchema )
