c7f83f5992e7cde5affee9331c81de06ce19a786
German Angarita <germanadolfoangarita@gmail.com>
2018-06-14 16:31:43 -0500

    Version 0.013

--- models/model.js
+++ models/model.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ModelSchema = new Schema ({
-    model: { type: String, unique: true },
+    model: { type: String },
     description: { type: String },
-    sixDigit: { String: String }
+    sixDigit: { type: String, unique: true},
+    img: {type: String}
 })
 
 module.exports = mongoose.model('Model', ModelSchema)
