d13b160c7cc31ff1f2805621ed8c1b2dde3555c4
German Angarita <germanadolfoangarita@gmail.com>
2018-06-15 08:26:09 -0500

    Version 0.013

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
+app.use(express.static('uploads/models'))
 app.use('/api', api)
 
 
